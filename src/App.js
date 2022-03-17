7a2ad9d7f051dadf6640c724e39c7e59731aa83b
Serotoninene <pujol.alexandre@hotmail.fr>
2022-03-17 10:39:09 +0100

    ejecting from webpack to install gsls loader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { Suspense, useRef } from "react";
+// Three
+import { TextureLoader } from "three";
+// R3F + Drei
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+// Assets
+import cakes from "./Assets/Images/cakes.jpg";
+import canap from "./Assets/Images/canap.jpg";
+import eliott from "./Assets/Images/eliott.jpg";
 // Styling
 import "./Scss/style.scss";
 
+function Scene() {
+  const spriteRef = useRef();
+  const texture = useLoader(TextureLoader, cakes);
+  console.log(spriteRef.current);
+  useFrame((state, delta) => {});
+  return (
+    <sprite ref={spriteRef} scale={[5, 5]}>
+      <spriteMaterial map={texture} />
+    </sprite>
+  );
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Canvas>
+        <OrbitControls />
+        <Suspense fallback={null}>
+          <Scene />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
